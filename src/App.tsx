04fab5a8779427cc69ed30bc4031121f69ff35e5
Achette <i.achette@live.com>
2023-02-19 19:43:37 -0300

    Chore: inicia desenvolvimento do componente ListingBody

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Header } from './components/Header';
+import { ListingBody } from './components/ListingBody';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <ListingBody />
     </div>
   );
 }
---
+++ src/components/ListingBody/index.tsx
@@ -0,0 +1,10 @@
+
+
+export const ListingBody = () => {
+
+    return(
+        <div>
+            Listing Body
+        </div>
+    )
+}
