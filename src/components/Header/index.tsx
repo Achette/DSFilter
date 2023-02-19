51892faad30530ebdc690618d4d6ff2970794c0d
Achette <i.achette@live.com>
2023-02-19 19:17:12 -0300

    Inicia projeto, remove arquivos desnecessários ao projeto e cria primeiro componente

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+
+
+export const Header = () => {
+
+    return (
+        <header>
+            <h1>DSFilter</h1>
+            <ul>
+                <li>6 produtos</li>
+            </ul>
+        </header>
+    )
+}
