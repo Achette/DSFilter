eb5c3934c5e7c1a0a3b5b43e09caf224022831e9
Achette <i.achette@live.com>
2023-02-19 19:38:58 -0300

    Finalizado componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,12 @@
-
+import "./styles.css";
 
 export const Header = () => {
-
-    return (
-        <header>
-            <h1>DSFilter</h1>
-            <ul>
-                <li>6 produtos</li>
-            </ul>
-        </header>
-    )
-}
+  return (
+    <header className="header-container">
+      <h1>DSFilter</h1>
+      <ul>
+        <li>6 produtos</li>
+      </ul>
+    </header>
+  );
+};
