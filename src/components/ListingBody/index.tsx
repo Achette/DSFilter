282cb849afcf220e5ea79d2c9578292e8367a3e9
Achette <i.achette@live.com>
2023-02-19 21:54:43 -0300

    adiciona lógica para buscar e exibir os produtos baseados no seu preço

--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -1,14 +1,49 @@
-import { CartFilter } from '../CartFilter'
-import { CartListing } from '../CartListing'
-import './styles.css'
-
+import React from "react";
+import { ProductDTO, QueryParams } from "../../types";
+import { CardFilter } from "../CardFilter";
+import { CartListing } from "../CartListing";
+import * as productService from "../../services/product-service";
+import "./styles.css";
+import { ContextNumberCount } from "../../context/context-product-count";
 
 export const ListingBody = () => {
+  const { setContexNumberCount } =
+    React.useContext(ContextNumberCount);
 
-    return(
-        <div className="listing-body-container">
-            <CartFilter />
-            <CartListing />
-        </div>
-    )
-}
+  const [products, setProducts] = React.useState<ProductDTO[]>([]);
+
+  const [queryParams, setQueryParams] = React.useState<QueryParams>({
+    minPrice: 0,
+    maxPrice: Number.MAX_VALUE,
+  });
+
+  React.useEffect(() => {
+    const loadProducts: ProductDTO[] = productService.findAll();
+    setProducts(loadProducts);
+
+    if (queryParams.minPrice !== 0 || queryParams.maxPrice !== 0) {
+      const filteredProducts: ProductDTO[] = productService.findByPrice(
+        queryParams.minPrice,
+        queryParams.maxPrice
+      );
+      setProducts(filteredProducts);
+
+      setContexNumberCount(
+        productService.findByPrice(queryParams.minPrice, queryParams.maxPrice)
+          .length
+      ); 
+    }
+  }, [queryParams.maxPrice, queryParams.minPrice, setContexNumberCount]);
+
+  const handleOnFilter = (minPrice: number, maxPrice: number) => {
+    setQueryParams({ ...queryParams, minPrice, maxPrice });
+  };
+  return (
+    <div className="listing-body-container">
+      <CardFilter onFilter={handleOnFilter} />
+      {products.map((product) => (
+        <CartListing key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
