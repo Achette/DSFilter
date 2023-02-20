8710a2cbfdc62342c386a69c3411203d7e376077
Achette <i.achette@live.com>
2023-02-19 21:55:53 -0300

    Adiciona lógica no Header para exibir quantidade de produtos dinâmica vinda do Context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
+import React from "react";
+import { ContextNumberCount } from "../../context/context-product-count";
 import "./styles.css";
 
 export const Header = () => {
+  const { contextNumberCount } = React.useContext(ContextNumberCount);
+
   return (
     <header className="header-container">
       <h1>DSFilter</h1>
       <ul>
-        <li>6 produtos</li>
+        <li>
+          {contextNumberCount > 1
+            ? `${contextNumberCount} produtos`
+            : `${contextNumberCount} produto`}
+        </li>
       </ul>
     </header>
   );
