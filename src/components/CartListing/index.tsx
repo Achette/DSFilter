adf8cc8cd5762f1e1bbf2610758a1e75720890c9
Achette <i.achette@live.com>
2023-02-19 21:56:32 -0300

    Refafora componente de CardListing

--- src/components/CartListing/index.tsx
+++ src/components/CartListing/index.tsx
@@ -1,10 +1,16 @@
-import './styles.css'
+import React from "react";
+import { CardListProps } from "../../types";
+import "./styles.css";
 
-export const CartListing = () => {
-
-    return (
-        <div className="cart-listing-container">
-            Cart Listing
+export const CartListing = ({ product }: CardListProps) => {
+  return (
+    <section className="">
+      <div className="cart-listing-container">
+        <div className="product-info">
+          <h3>{product.name}</h3>
+          <p>R$ {product.price.toFixed(2)}</p>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
